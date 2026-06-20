ed9e7d873985944d56dccedef7c30f3dd4179851
evalene20 <eshaevalene20@gmail.com>
2026-06-20 18:39:41 +0400

    changes in routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,10 @@ from app.api.auth import router
 
 from app.models.user import User
 
+from app.api.interview import router as interview_router
+
+from app.api.upload import router as upload_router
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -18,3 +22,13 @@ def home():
     return {
         "message": "API Running"
     }
+
+
+
+app.include_router(
+    interview_router
+)
+
+app.include_router(
+    upload_router
+)
